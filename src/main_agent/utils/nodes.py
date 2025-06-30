e4ebbdcae1c99484335468be1024fd56e273b661
lifecheckpoint <lifechpt@qq.com>
2025-06-30 17:07:12 +0800

    welcome节点逻辑

--- src/main_agent/utils/nodes.py
+++ src/main_agent/utils/nodes.py
@@ -7,9 +7,10 @@ Agent 将在这些节点之间进行切换，处理任务
 from __future__ import annotations
 
 from langchain_core.runnables import RunnableConfig
+from langchain_core.messages import SystemMessage
+from pathlib import Path
 from typing import Any, Dict
 
-from conf import Configuration
 from state import MainAgentState
 
 
@@ -17,6 +18,26 @@ async def welcome(state: MainAgentState, config: RunnableConfig) -> Dict[str, An
     """
     欢迎节点，在该节点中注入系统提示词与欢迎信息
     """
+    # 读取提示词
+    system_prompt_path = Path(__file__).parent / "instruction.txt"
     
+    if not system_prompt_path.exists():
+        # TODO error node
+        pass
+
+    try:
+        system_prompt = system_prompt_path.read_text(encoding="utf-8")
+
+        # 替换模板
+        system_prompt = system_prompt.format_map({
+            "user_instruction": config.get("configurable", {}).extra_system_prompt,
+        })
+
+    except Exception as e:
+        # TODO error node
+        pass
+
+    # 注入系统提示词
+    return {"messages": [SystemMessage(content=system_prompt)]}
 
 __all__ = ["welcome"]
