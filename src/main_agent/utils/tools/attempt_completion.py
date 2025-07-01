5fea4e0be84d422b809a3e35c6b47d65f18fc60a
lifecheckpoint <lifechpt@qq.com>
2025-07-01 13:07:39 +0800

    优化attempt_completion提示词

--- src/main_agent/utils/tools/attempt_completion.py
+++ src/main_agent/utils/tools/attempt_completion.py
@@ -4,6 +4,16 @@ from langchain_core.tools import tool
 class AttemptCompletionSchema(BaseModel):
     """
     当你认为一个任务完成的时候，使用此工具向系统确认该次任务完成
+
+    Example:
+
+    1. 已经成功解答了用户的问题
+    - status: 解答完成
+    - reason: 通过查询知识库和互联网，成功找到并提供了亚特兰蒂斯的地理信息与相关历史背景，同时更新了用户的知识库
+
+    2. 由于某些原因导致任务失败
+    - status: 任务失败
+    - reason: 由于网络问题，无法访问相关资源，已向用户说明情况并建议稍后重试
     """
     status: str = Field(description="任务完成状态，例如“成功”或“失败”，保持简短")
     reason: str = Field(description="任务完成情况描述，描述该任务通过什么方式被完成，最终结果是什么")
