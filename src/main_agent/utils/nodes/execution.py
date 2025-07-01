0d3dfefbc69a7282e7aa10885b521800282a71da
lifecheckpoint <lifechpt@qq.com>
2025-07-01 12:53:09 +0800

    更换了一个模型，新增测试

--- src/main_agent/utils/nodes/execution.py
+++ src/main_agent/utils/nodes/execution.py
@@ -14,14 +14,14 @@ async def agent_execution(state: MainAgentState, config: RunnableConfig) -> Dict
     Agent 执行节点，处理用户输入并执行任务
     """
     llm = ChatOpenAI(
-        model="google/gemini-2.5-flash",
-        temperature=0.5,
+        model="deepseek/deepseek-chat-v3-0324",
+        temperature=0.3,
         base_url="https://openrouter.ai/api/v1",
         api_key=(await (AsyncPath(__file__).parent.parent / "api_key").read_text(encoding="utf-8")).strip(),
         max_retries=3,
         max_tokens=8192,
-        frequency_penalty=0.5,
+        # frequency_penalty=0.4,
     ).bind_tools(tool_list) # TODO: 更灵活的模型配置
     
-    response = await llm.ainvoke(state.messages)
+    response = llm.invoke(state.messages)
     return {"messages": [response]}
