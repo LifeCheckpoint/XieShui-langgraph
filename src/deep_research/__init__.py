c589744428ecdb5e86338da6e9fcf188343b9d59
lifecheckpoint <lifechpt@qq.com>
2025-07-19 18:42:39 +0800

    导出模块

--- src/deep_research/__init__.py
+++ src/deep_research/__init__.py
@@ -2,7 +2,6 @@
 导入状态、节点与工具
 """
 
-from src.main_agent.utils.conf import *
-from src.main_agent.utils.tools import *
-from src.main_agent.utils.nodes import *
-from src.main_agent.utils.state import *
+from src.deep_research.graph import builder
+
+__all__ = ["builder"]
