45248afd5890b839d7b26a637d7ea8b1d8eee57c
lifecheckpoint <lifechpt@qq.com>
2025-07-23 17:53:42 +0800

    图构建

--- src/graph_manager/__init__.py
+++ src/graph_manager/__init__.py
@@ -2,6 +2,6 @@
 导入状态、节点与工具
 """
 
-# from src.graph_manager.graph import builder
+from src.graph_manager.utils.graph import graph_manager_builder
 
-__all__ = ["builder"]
+__all__ = ["graph_manager_builder"]
