ed00334acadeb6da4b6a1722efe9c3d417db9159
lifecheckpoint <lifechpt@qq.com>
2025-07-23 17:53:19 +0800

    工具导入

--- src/graph_manager/utils/tools/__init__.py
+++ src/graph_manager/utils/tools/__init__.py
@@ -2,8 +2,12 @@ from langgraph.prebuilt import ToolNode
 
 from src.graph_manager.utils.tools.attempt_completion import attempt_completion
 from src.graph_manager.utils.tools.klgraph_abort import klgraph_abort
+from src.graph_manager.utils.tools.graph_management import management_tool_list
+from src.graph_manager.utils.tools.graph_reading import reading_tool_list
+from src.graph_manager.utils.tools.graph_writing import writing_tool_list
 
-tool_list = [attempt_completion, klgraph_abort]
+# 合并所有工具列表
+tool_list = [attempt_completion, klgraph_abort] + management_tool_list + reading_tool_list + writing_tool_list
 """
 工具列表，包含了所有可用的工具函数
 """
